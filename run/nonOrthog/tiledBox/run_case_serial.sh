5d76c00a57ffe1f98520e1a713201d0983fe27be
Jun Liu <babygod1993@gmail.com>
2025-07-23 14:58:39 +0200

    Update case setup for non-orthogonal paper.

--- run/nonOrthog/tiledBox/run_case_serial.sh
+++ run/nonOrthog/tiledBox/run_case_serial.sh
@@ -7,5 +7,5 @@ SOLVER=$2
 create-parameter-study.py -p "$SOLVER" -t template "$PARAM_FILE_PREFIX".parameter  && \
 bulkeval "$SOLVER"-"$PARAM_FILE_PREFIX"_000 "sed -i '1 i\#!/bin/bash' interFlow.sbatch"
 bulkeval "$SOLVER"-"$PARAM_FILE_PREFIX"_000 "sed -i '1 i\#!/bin/bash' initFields.sh"
-initilize-parameter-study.py "$SOLVER"-"$PARAM_FILE_PREFIX"_000 -m blockMesh -f initFields.sh
+initialize-parameter-study.py "$SOLVER"-"$PARAM_FILE_PREFIX"_000 -m blockMesh -f initFields.sh
 
